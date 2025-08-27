fd1abe31c55e51bb44462df45481eedeef384fda
Sébastien Piérard <Sebastien.Pierard@gmail.com>
2025-08-27 10:04:07 +0100

    refactoring

--- sorbetto/__init__.py
+++ sorbetto/__init__.py
@@ -2,4 +2,10 @@
 
 __version__ = '0.0.0'
 
-print ( "Hi! This is OURSUPERLIBRARY {},".format ( __version__ ) )
+print ( "Hi! This is Sorbetto {},".format ( __version__ ) )
+
+
+from sorbetto.AbstractParameterization import *
+from sorbetto.ParameterizationDefault import *
+from sorbetto.ParameterizationAdaptedToClassPriors import *
+from sorbetto.ParameterizationAdaptedToPredictionRates import *
---
+++ sorbetto/parametrization/ParameterizationAdaptedToPredictionRates.py
@@ -0,0 +1,58 @@
+
+    
+class ParameterizationAdaptedToPredictionRates ( AbstractParameterization ) :
+    """
+    Not yet published. Experimental. In Sébastien's mind.
+    
+    Using the parameterization adapted to prediction rates with performances corresponding
+    to the prediction rates $(\tau_-,\tau_+)$ is equivalent to using the default
+    parameterization after applying a target shift operation
+    :cite:t:`Sipka2022TheHitchhikerGuide` on all performances in order to balance the
+    prediction rates.
+    
+    See Theorem 6 of future "paper 6".
+    """
+    
+    def __init__ ( self, ratePos ) :
+        assert isinstance ( ratePos, float )
+        assert ratePos >= 0.0
+        assert ratePos <= 1.0
+        self._ratePos = ratePos
+        self._ratePos = 1 - ratePos
+        AbstractParameterization.__init__ ( self )
+    
+    def getRateOfNegativePredictions () -> float :
+        return self._rateNeg
+    
+    def getRateOfPositivePredictions () -> float :
+        return self._ratePos
+
+    
+    def getNameParameter1 ( self )
+
+    
+    def getNameParameter2 ( self )
+
+    
+
+    def getBoundsParameter1 ( self ) -> tuple[float, float] :
+        return 0.0, 1.0
+
+    def getBoundsParameter2 ( self ) -> tuple[float, float] :
+        return 0.0, 1.0
+
+    
+    def getCanonicalRankingScore ( self, param1, param2 ) -> RankingScore
+
+    
+    def getValueParameter1 ( self, rankingScore ) -> float
+
+    
+    def getValueParameter2 ( self, rankingScore ) -> float
+
+    
+    def getName ( self ) :
+        return 'adapted to prediction rates (pos: {:g})'.format ( self._ratePos )
+
+
+
