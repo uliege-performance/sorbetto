7c8cca14974b67bf33530cfdc778114d0a888a7a
Sébastien Piérard <Sebastien.Pierard@gmail.com>
2025-08-29 16:41:25 +0100

    [ADD] Add class AnnotationGeometric

---
+++ sorbetto/annotation/annotation_geometric.py
@@ -0,0 +1,38 @@
+from sorbetto.annotation.abstract_annotation import AbstractAnnotation
+from sorbetto.geometry.abstract_geometric_object_2d import AbstractGeometricObject2D
+from sorbetto.tile.asbtract_tile import AbstractTile
+
+
+class AnnotationGeometric(AbstractAnnotation):
+    """
+    This type of annotation can be used with any fixed geometric object.
+    """
+
+    def __init__(
+        self, geom: AbstractGeometricObject2D, name: str | None = None, **plt_kwargs
+    ):
+        """
+        Initializes a new annotation for a geometric object.
+
+        Args:
+            geom (AbstractGeometricObject2D): the geometric object.
+            name (str | None, optional): the annotation name.
+        """
+
+        assert isinstance(geom, AbstractGeometricObject2D)
+        self._geom = geom
+
+        self._plt_kwargs = plt_kwargs
+
+        AbstractGeometricObject2D.__init__(self)
+
+    def draw(self, tile: AbstractTile, fig, ax) -> None:
+        assert isinstance(tile, AbstractTile)
+        parameterization = tile.getParameterization()
+        min1, max1 = parameterization.getBoundsParameter1()
+        min2, max2 = parameterization.getBoundsParameter2()
+        extent = [min1, max1, min2, max2]
+        self._geom.draw(fig, ax, extent, self._plt_kwargs)
+
+    def __str__(self) -> str:
+        return self.name
