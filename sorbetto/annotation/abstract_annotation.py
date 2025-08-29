f0ff958735b60562aa8bb1de3bfdaa8cdc3db41c
Sébastien Piérard <Sebastien.Pierard@gmail.com>
2025-08-29 16:06:30 +0100

    Create abstract_annotation.py

---
+++ sorbetto/annotation/abstract_annotation.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractAnnotation(ABC):
+    """
+    This is the base class for all annotations, which are things that are drawn on top of Tiles.
+    """
+
+    def __init__(self, name: str | None = None):
+        """
+        Initializes a new annotation.
+
+        Args:
+            name (str | None, optional): the annotation name.
+        """
+        if name is None:
+            name = "unnamed annotation"
+        else:
+            if not isinstance(name, str):
+                name = str(name)
+        self._name = name
+        ABC.__init__(self)
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @abstractmethod
+    def draw(self, tile, fig, ax) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return self.name
