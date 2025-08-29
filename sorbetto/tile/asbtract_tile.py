cdca46412a116b02bb7fb6912c764493a2c7c939
Jérôme PIERRE <jepi1202@gmail.com>
2025-08-29 11:06:10 +0200

    [ADD] initial structure tile

---
+++ sorbetto/tile/asbtract_tile.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+from sorbetto.parametrization.abstract_parametrization import AbstractParameterization
+from sorbetto.flavor.abstract_flavor import AbstractFlavor
+from numpy import ndarray
+
+
+class AbstractTile(ABC):
+    """
+    This is the base class for all Tiles.
+    Tiles with the default parameterization are studied in detail in :cite:t:`Pierard2024TheTile-arxiv`.
+    Various flavors of Tiles are described in :cite:t:`Halin2024AHitchhikers-arxiv` and :cite:t:`Pierard2025AMethodology`.
+    """
+
+    def __init__(self, name, parameterization, flavor, resolution=1001):
+        self._name = name
+        self._parameterization = parameterization
+        self._flavor = flavor
+        self._resolution = resolution
+        # other args
+        ...  # TODO
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def parameterization(self) -> AbstractParameterization:
+        return self._parameterization
+
+    @property
+    def flavor(self) -> AbstractFlavor:
+        return self._flavor
+
+    @property
+    def resolution(self) -> int:
+        return self._resolution
+
+    @abstractmethod
+    def getExplanation(self) -> str: ...  # TODO
+
+    def getParameterization(self) -> AbstractParameterization: ...  # TODO
+
+    def getFlavor(self) -> AbstractFlavor: ...  # TODO
+
+    def getResolution(self) -> int: ...  # TODO
+
+    def getVecParam1(self) -> ndarray: ...  # TODO
+
+    def getVecParam2(self) -> ndarray: ...  # TODO
+
+    def getMat(self) -> ndarray: ...  # TODO
+
+    def getColormap(self) -> ndarray: ...  # TODO
+
+    # annotations : List [ Annotation ]
+
+    def genAnnotations(self):  # generator
+        ...  # TODO
+
+    def addAnnotation(self, annotation): ...  # TODO
+
+    def delAnnotation(self, annotation): ...  # TODO
+
+    @abstractmethod
+    def draw(self, fig, ax): ...  # TODO
+
+    def __call__(self, param1, param2):  # uses `flavor ( importances )`.
+        ...  # TODO
+
+    def __str__(self) -> str: ...  # TODO
