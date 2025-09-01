ee21bcd5b14f44f4669528243fd6c7641aa0ac9f
Jérôme PIERRE <jepi1202@gmail.com>
2025-09-02 01:31:36 +0200

    [UPDATE] analysis method designer

--- sorbetto/analysis/analysis_method_designer.py
+++ sorbetto/analysis/analysis_method_designer.py
@@ -1,35 +1,90 @@
+import numpy as np
+
 from sorbetto.analysis.abstract_analysis import AbstractAnalysis
-from sorbetto.tile.abstract_numeric_tile import AbstractNumericTile
+from sorbetto.flavor.baseline_flavor import BaselineFlavor
+from sorbetto.flavor.sota_flavor import SOTAFlavor
+from sorbetto.flavor.value_flavor import ValueFlavor
+from sorbetto.parameterization.abstract_parameterization import AbstractParameterization
+from sorbetto.performance.finite_set_of_two_class_classification_performances import (
+    FiniteSetOfTwoClassClassificationPerformances,
+)
+from sorbetto.tile.abstract_numerical_tile import AbstractNumericalTile
 from sorbetto.tile.value_tile import ValueTile
 
 
 class AnalysisForMethodDesigner(AbstractAnalysis):
     def __init__(
         self,
-        performance,
+        performance: FiniteSetOfTwoClassClassificationPerformances,
         competitors,
-        parameterization,
-        resolution=1001,
+        parameterization: AbstractParameterization,
+        resolution: int = 1001,
         **kwargs,
     ):
-        ...  # TODO
+        self._parameterization = parameterization
+        self._performance = performance  # what is that ?
+
+        print("Why competitors?")
+
         AbstractAnalysis.__init__(self, parameterization, resolution)
+
+    @property
+    def parameterization(self):
+        return self._parameterization
+
+    @property
+    def performance(self):
+        return self._performance
 
     def getNoSkillTile(
         self,
-    ) -> AbstractNumericTile:  # See :cite:t:`Pierard2024TheTile-arxiv`, Figure 9.
+    ) -> AbstractNumericalTile:  # See :cite:t:`Pierard2024TheTile-arxiv`, Figure 9.
         ...  # TODO
 
-    def getBaselineValueTile(self) -> AbstractNumericTile: ...  # TODO
+    def getBaselineValueTile(self) -> AbstractNumericalTile:
+        flavor = BaselineFlavor()
+        values = ValueTile(
+            performance=self._performance,
+            parameterization=self._parameterization,
+            flavor=flavor,
+            resolution=self._resolution,
+            name="Baseline Value Tile",
+        )
 
-    def getStateOfTheArtValueTile(self) -> AbstractNumericTile: ...  # TODO
+        return np.min(values, axis=0)
 
-    def getValueTile(self) -> ValueTile: ...  # TODO
+    def getStateOfTheArtValueTile(self) -> AbstractNumericalTile:
+        flavor = SOTAFlavor()
+        values = ValueTile(
+            performance=self._performance,
+            parameterization=self._parameterization,
+            flavor=flavor,
+            resolution=self._resolution,
+            name="SOTA Value Tile",
+        )
 
-    def getRankingTile(self) -> AbstractNumericTile: ...  # TODO
+        return np.max(values, axis=0)
+
+    def getValueTile(self, id) -> ValueTile:
+        flavor = ValueFlavor()
+        values = ValueTile(
+            performance=self._performance[id],
+            parameterization=self._parameterization,
+            flavor=flavor,
+            resolution=self._resolution,
+            name="Value Tile",
+        )
+
+        return values
+
+    def getRankingTile(self) -> AbstractNumericalTile: ...  # TODO
 
     def drawInROC(self, fig, ax):  # and options ?
         ...  # TODO
 
     def getAdvice(self, fmt) -> str:  # fmt can be: txt, html, latex
         ...  # TODO
+
+    def genTiles(self):
+        # TODO: discuss if this should retieve the SOTA tile
+        return self.getStateOfTheArtValueTile()
