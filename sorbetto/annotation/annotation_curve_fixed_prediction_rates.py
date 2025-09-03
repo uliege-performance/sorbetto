ae841b37a618884d8fcc7fb3f87f5787718664fc
Sébastien Piérard <Sebastien.Pierard@gmail.com>
2025-09-03 17:47:04 +0100

    Create annotation_curve_fixed_prediction_rates.py

---
+++ sorbetto/annotation/annotation_curve_fixed_prediction_rates.py
@@ -0,0 +1,49 @@
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+
+from sorbetto.annotation.abstract_annotation import AbstractAnnotation
+from sorbetto.performance.constraint_fixed_prediction_rates import (
+    ConstraintFixedPredictionRates,
+)
+from sorbetto.tile.tile import Tile
+
+
+class AnnotationCurveFixedPredictionRates(AbstractAnnotation):
+    def __init__(
+        self,
+        ratePos: float | ConstraintFixedPredictionRates,
+        name: str | None = None,
+        **plt_kwargs,
+    ):
+        if isinstance(ratePos, ConstraintFixedPredictionRates):
+            ratePos = ratePos.getRatePos()
+        assert isinstance(ratePos, float)
+        assert ratePos > 0.0
+        assert ratePos < 1.0
+        self._ratePos = ratePos
+
+        if name is None:
+            name = "locus of performance orderings putting all no-skill performances with the prediction rates ({:g}, {:g}) on an equal footing".format(
+                1.0 - ratePos, ratePos
+            )
+        else:
+            if not isinstance(name, str):
+                name = str(name)
+
+        self._plt_kwargs = plt_kwargs
+
+        AbstractAnnotation.__init__(self, name)
+
+    def draw(self, tile: Tile, fig: Figure, ax: Axes) -> None:
+        assert isinstance(tile, Tile)
+
+        parameterization = tile.parameterization
+        min1, max1 = parameterization.getBoundsParameter1()
+        min2, max2 = parameterization.getBoundsParameter2()
+        extent = [min1, max1, min2, max2]
+
+        curve = parameterization.locateOrderingsPuttingNoSkillPerformancesOnAnEqualFootingForFixedPredictionRates(
+            self._ratePos
+        )
+
+        curve.draw(fig, ax, extent, **self._plt_kwargs)
