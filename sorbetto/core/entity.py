3cfe0ff7a5b5eccf4113f055fc137378f0133d05
Anaïs Halin <anais.halin@uliege.be>
2025-09-04 23:53:46 +0100

    [IMP] modify default color

--- sorbetto/core/entity.py
+++ sorbetto/core/entity.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+import numpy as np
+
 from sorbetto.performance.two_class_classification_performance import (
     TwoClassClassificationPerformance,
 )
@@ -10,17 +12,19 @@ class Entity:
         self,
         performance: TwoClassClassificationPerformance,
         name: str = "ε",
-        color: Any = "blue",
+        color: Any = None,
     ):
         """Entity class
 
         Args:
             performance (TwoClassClassificationPerformance): Performance score for the entity
             name (str): Name of the entity. Defaults to "ε"
-            color (Any, optional): Color to use for the entity. Defaults to "blue".
+            color (Any, optional): Color to use for the entity. Defaults to a random list of floats.
         """
 
         self._name = name
+        if color is None:
+            color = list(np.random.random(3))
         self._color = color
         self._performance = performance
 
