7b5cfa17083b26b887164f1545c7c9157c1d0f55
Jérôme PIERRE <jepi1202@gmail.com>
2025-09-04 11:47:52 +0200

    [ADD] refactoring analysis

--- sorbetto/analysis/analysis_app_developer.py
+++ sorbetto/analysis/analysis_app_developer.py
@@ -1,10 +1,17 @@
+import os
+
+import matplotlib.pyplot as plt
+
 from sorbetto.analysis.abstract_analysis import AbstractAnalysis
 from sorbetto.core.entity import Entity
 from sorbetto.flavor.entity_flavor import EntityFlavor
 from sorbetto.flavor.value_flavor import ValueFlavor
 from sorbetto.parameterization.abstract_parameterization import AbstractParameterization
+from sorbetto.performance.finite_set_of_two_class_classification_performances import (
+    FiniteSetOfTwoClassClassificationPerformances,
+)
 from sorbetto.tile.entity_tile import EntityTile
-from sorbetto.tile.symbolic_tile import AbstractSymbolicTile
+from sorbetto.tile.tile import Tile
 from sorbetto.tile.value_tile import ValueTile
 
 
@@ -17,34 +24,95 @@ class AnalysisForAppDeveloper(AbstractAnalysis):
         **kwargs,
     ):
         self._entities = entities
+        self._performances = FiniteSetOfTwoClassClassificationPerformances(
+            [entity.performance for entity in entities]
+        )
 
         AbstractAnalysis.__init__(self, parameterization, resolution)
 
-    def getEntityTile(self, rank) -> AbstractSymbolicTile:
-        flavor = EntityFlavor()
+    @property
+    def entities(self) -> list[Entity]:
+        return self._entities
+
+    @entities.setter
+    def entities(self, value: list[Entity]):
+        self._entities = value
+
+    @property
+    def performances(self) -> FiniteSetOfTwoClassClassificationPerformances:
+        return self._performances
+
+    @performances.setter
+    def performances(self, value: FiniteSetOfTwoClassClassificationPerformances):
+        self._performances = value
+
+    def getEntityTile(self, rank) -> EntityTile:
+        flavor = EntityFlavor(rank=rank, entity_list=self.entities)
 
         entity_tile = EntityTile(
-            entities_list=self._entities,
             parameterization=self._parameterization,
             flavor=flavor,
             resolution=self._resolution,
             name=f"Entity Tile for rank {rank}",
-            rank=rank,
         )
 
         return entity_tile
 
     def getValueTile(self, entity: Entity) -> ValueTile:
-        # TODO: choose if entity or entity_name /entity_id
-        flavor = ValueFlavor()
         performance = entity.performance
+        flavor = ValueFlavor(performance=performance)
         return ValueTile(
-            performance=performance,
             flavor=flavor,
             parameterization=self._parameterization,
             resolution=self._resolution,
             name="Value Tile",
         )
+
+    def getCorrelationTile(self, score, correlation_fct):
+        performances = self.performances
+        return self._getCorrelationTile(
+            performances=performances,
+            correlation_fct=correlation_fct,
+            name="Correlation Tile",
+            score=score,
+        )
+
+    def getRankingTile(self, entity: Entity, rank: int):
+        return self._getRankingTile(
+            entity=entity,
+            entitiy_list=self.entities,
+            name=f"Ranking Tile for rank {rank}",
+        )
+
+    def saveAllValueTiles(self, save_dir: str | None = None):
+        if save_dir is None:
+            raise ValueError("save_dir must be specified")
+
+        os.makedirs(save_dir, exist_ok=True)
+
+        for entity in self.entities:
+            value_tile = self.getValueTile(entity)
+
+            filename = os.path.join(save_dir, f"value_tile_{entity.name}.png")
+            fig, ax = value_tile.draw()
+
+            plt.savefig(filename)
+            plt.close()
+
+    def saveAllEntityTiles(self, save_dir: str | None = None):
+        if save_dir is None:
+            raise ValueError("save_dir must be specified")
+
+        os.makedirs(save_dir, exist_ok=True)
+
+        for rank in range(len(self.entities)):
+            entity_tile = self.getEntityTile(rank)
+
+            filename = os.path.join(save_dir, f"entity_tile_{rank}.png")
+            fig, ax = entity_tile.draw()
+
+            plt.savefig(filename)
+            plt.close()
 
     def getAdvice(self, fmt) -> str:  # fmt can be: txt, html, latex
         ...  # TODO
