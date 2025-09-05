459d192481c7de2df5b25a3dac1c704b4d4efeec
Sébastien Piérard <Sebastien.Pierard@gmail.com>
2025-09-05 13:35:26 +0200

    [DEL] Delete drafts

--- sorbetto/parameterization/__init__.py
+++ sorbetto/parameterization/__init__.py
@@ -1,15 +1,7 @@
 from .abstract_parameterization import AbstractParameterization
-from .parameterization_adapted_to_class_priors import (
-    ParameterizationAdaptedToClassPriors,
-)
-from .parameterization_adapted_to_prediction_rates import (
-    ParameterizationAdaptedToPredictionRates,
-)
 from .parameterization_default import ParameterizationDefault
 
 __all__ = [
     "AbstractParameterization",
-    "ParameterizationAdaptedToClassPriors",
-    "ParameterizationAdaptedToPredictionRates",
     "ParameterizationDefault",
 ]
