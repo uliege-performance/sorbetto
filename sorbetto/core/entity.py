175b5c24e2e4315c625f3166cacf3beaf8f2bcd3
M. Francois <10929150+MarelliF@users.noreply.github.com>
2025-09-08 13:13:36 +0200

    [REF] add notebooks to ruff pre-commit

--- sorbetto/core/entity.py
+++ sorbetto/core/entity.py
@@ -8,9 +8,6 @@ from sorbetto.performance.two_class_classification_performance import (
 
 
 class Entity:
-
-
-    
     def __init__(
         self,
         performance: TwoClassClassificationPerformance,
