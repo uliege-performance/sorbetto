bdb5861d72dc70b0b4eeb4e8c9addc87eb656cb9
Sébastien Piérard <Sebastien.Pierard@gmail.com>
2025-09-09 13:02:15 +0200

    Update roc.py

--- sorbetto/performance/roc.py
+++ sorbetto/performance/roc.py
@@ -1,17 +1,23 @@
 # Copyright (c) 2025-2025, Sebastien Pierard et al.
 # SPDX-License-Identifier: Apache-2.0
 
+import logging
 import math
+
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
 
 def _setupROC(
-    fig,
-    ax,
+    fig: Figure,
+    ax: Axes,
     priorPos: float | None = None,
     show_no_skills: bool = True,
     show_priors: bool = True,
     show_unbiased=True,
 ):
+    assert isinstance(fig, Figure)
+    assert isinstance(ax, Axes)
     if priorPos is not None:
         assert isinstance(priorPos, float)
         assert priorPos >= 0.0
@@ -25,6 +31,15 @@ def _setupROC(
     assert isinstance(show_no_skills, bool)
     assert isinstance(show_priors, bool)
     assert isinstance(show_unbiased, bool)
+
+    if priorNeg < 1e-8:
+        message = "The prior of the negative class is {:g}".format(priorNeg)
+        message += "ROC coordinates are unreliable with such a low value."
+        logging.warning(message)
+    if priorNeg < 1e-8:
+        message = "The prior of the positive class is {:g}".format(priorPos)
+        message += "ROC coordinates are unreliable with such a low value."
+        logging.warning(message)
 
     if show_no_skills:
         ax.plot([0, 1], [0, 1], "--", c="palevioletred")
@@ -42,6 +57,7 @@ def _setupROC(
         ax.plot(
             [0, priorPos, priorPos], [priorPos, priorPos, 0], ":", c="palevioletred"
         )
+        ax.plot(priorPos, priorPos, "o", c="palevioletred")
 
     if show_unbiased:
         if priorPos <= 0.5:
